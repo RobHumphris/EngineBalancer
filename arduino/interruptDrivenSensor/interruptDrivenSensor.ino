d97ccbd8808af09f702ad6361a1e13d96db8b836
Rob Humphris <Rob Humphris>
2017-06-10 19:56:41 +0100

    Tested changes to Arduino code

--- arduino/interruptDrivenSensor/interruptDrivenSensor.ino
+++ arduino/interruptDrivenSensor/interruptDrivenSensor.ino
@@ -4,10 +4,14 @@ const int time_step = 500 ; // reading every 0.5s
 const int triggerPin = 2;
 const int directionPin = 3;
 const int syncPin = 4;
-const int triggerTeeth = 4; // 180;
+const int debugPin = 13;
+const int triggerTeeth = 360;
 
+unsigned long revs = 0; 
 long time = 0;
 unsigned long previousTimeStamp = micros();
+unsigned long prevSync = 9999;
+unsigned long _rpm = 9999;
 
 bool synced = false;
 long toothCount = 0;
@@ -20,40 +24,56 @@ WheatstoneBridge wsbStrainB(A1, 365, 675, 0, 1000);
 
 void setup() {
   pinMode(triggerPin, INPUT);
-  digitalWrite(triggerPin, HIGH);
+  digitalWrite(triggerPin, LOW);
 
   pinMode(directionPin, INPUT);
-  digitalWrite(directionPin, HIGH);
+  digitalWrite(directionPin, LOW);
 
   pinMode(syncPin, INPUT);
-  digitalWrite(syncPin, HIGH);
+  digitalWrite(syncPin, LOW);
+
+  pinMode(debugPin, OUTPUT);
+  digitalWrite(debugPin, LOW);
   
-  attachInterrupt(digitalPinToInterrupt(triggerPin), triggerPinInterrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(triggerPin), triggerPinInterrupt, FALLING);
   Serial.begin(9600); //  setup serial baudrate
   Serial.println("Setup Complete.");
 }
 
 void loop() {
-  int strain = wsbStrainA.measureForce();
-  int raw = wsbStrainA.getLastForceRawADC();
+  //int strain = wsbStrainA.measureForce();
+  //int raw = wsbStrainA.getLastForceRawADC();
   
   if(millis() > time_step+time) {
-    Serial.print("Synced: ");
+    /*Serial.print("Synced: ");
     Serial.print(synced);
-    
-    Serial.print("\tTooth Count: ");
+
+    Serial.print("    ");*/
+    Serial.print("Tooth Count: ");
     Serial.print(toothCount);
+
+    Serial.print("    ");
+    Serial.print("Revs: ");
+    Serial.print(revs);
+
     
-    Serial.print("\tWheel Direction: ");
+    Serial.print("    ");
+    Serial.print("RPM: ");
+    Serial.print(_rpm);
+    
+    /*Serial.print("    ");
+    Serial.print("Wheel Direction: ");
     Serial.print(wheelDirection);
-
-    Serial.print("\tStrain A: ");
-    Serial.print(strain);
-
-    Serial.print("\tRaw A: ");
-    Serial.print(raw);
     
-    Serial.println('\n'); 
+    Serial.print("    ");
+    Serial.print("Strain A: ");
+    Serial.print(strain);
+    
+    Serial.print("    ");
+    Serial.print("Raw A: ");
+    Serial.print(raw);*/
+    
+    Serial.print('\n'); 
     time = millis();
   }
 }
@@ -62,8 +82,15 @@ void checkSyncPin() {
   int sync = digitalRead(syncPin);
   if (sync == LOW) {
     synced = true;
-    toothCount = 0;
+    //toothCount = 0;
   }
+}
+
+void countRevs() {
+  unsigned long time = millis();
+  _rpm = 60000 / (time - prevSync);
+  revs++;
+  prevSync = time;
 }
 
 void checkDirection() {
@@ -72,8 +99,10 @@ void checkDirection() {
   toothCount += wheelDirection;
   if (toothCount < 0) {
     toothCount = triggerTeeth;
+    countRevs();
   } else if (toothCount > triggerTeeth) {
     toothCount = 0;
+    countRevs();
   }
 }
 
@@ -83,12 +112,15 @@ void  getStrainReadings() {
 }
 
 void triggerPinInterrupt() {
-  unsigned long timestamp = micros();
-  if (synced) {
-    checkDirection();
-    getStrainReadings();
-  } 
-  checkSyncPin();
+  unsigned long timestamp = millis();
+  digitalWrite(debugPin, HIGH);
+ 
+  //if (synced) {
+  checkDirection();
+  //  getStrainReadings();
+  //} 
+  //checkSyncPin();
   previousTimeStamp = timestamp;
+  digitalWrite(debugPin, LOW);
 }
 
